abc0ca91e33e1097b62bfb1bb6f29374cbbd3c4b
Annas Sovianto <141797683+Reigentsu@users.noreply.github.com>
2025-06-02 20:58:13 +0700

    update main session

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'firebase_options.dart';
 import 'views/auth_wrapper.dart';
+import 'models/favorite_model.dart';
+import 'utils/hive_box.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Initialize Hive
+  await Hive.initFlutter();
+
+  // Register Hive adapters
+  Hive.registerAdapter(FavoriteQuestAdapter());
+
+  // Open Hive boxes
+  await Hive.openBox<FavoriteQuest>(HiveBox.favorites);
+
   runApp(const MainApp());
 }
 
